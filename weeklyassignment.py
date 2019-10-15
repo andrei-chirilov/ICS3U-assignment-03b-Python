72e0a80c99ae9400b2f8b15c809f9db1a3edf0dc
Ubuntu <ubuntu@ip-172-31-28-78.ec2.internal>
2019-10-15 18:17:23 +0000

    c

---
+++ weeklyassignment.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+
+# Created by: Andrei Chirilov
+# Created on: Oct 2019
+# This program checks to see if a letter is a vowel, consonant, or both
+
+
+def main():
+
+    letter = input("Enter a letter: ")
+
+    if (letter == "A"):
+        print("This is a vowel")
+    elif (letter == "E"):
+        print("This is a vowel")
+    elif (letter == "I"):
+        print("This is a vowel")
+    elif (letter == "O"):
+        print("This is a vowel")
+    elif (letter == "U"):
+        print("This is a vowel")
+    elif (letter == "B"):
+        print("This is a consonant")
+    elif (letter == "C"):
+        print("This is a consonant")
+    elif (letter == "D"):
+        print("This is a consonant")
+    elif (letter == "F"):
+        print("This is a consonant")
+    elif (letter == "G"):
+        print("This is a consonant")
+    elif (letter == "H"):
+        print("This is a consonant")
+    elif (letter == "J"):
+        print("This is a consonant")
+    elif (letter == "K"):
+        print("This is a consonant")
+    elif (letter == "L"):
+        print("This is a consonant")
+    elif (letter == "M"):
+        print("This is a consonant")
+    elif (letter == "N"):
+        print("This is a consonant")
+    elif (letter == "P"):
+        print("This is a consonant")
+    elif (letter == "Q"):
+        print("This is a consonant")
+    elif (letter == "R"):
+        print("This is a consonant")
+    elif (letter == "S"):
+        print("This is a consonant")
+    elif (letter == "T"):
+        print("This is a consonant")
+    elif (letter == "V"):
+        print("This is a consonant")
+    elif (letter == "W"):
+        print("This is a consonant")
+    elif (letter == "X"):
+        print("This is a consonant")
+    elif (letter == "Z"):
+        print("This is a consonant")
+    elif (letter == "Y"):
+        print("This is both")
+    else:
+        print("This is not a letter")
+
+
+if __name__ == "__main__":
+    main()
